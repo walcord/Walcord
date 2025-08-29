6b0498c40657735504a28e93566766f36ccc627a
Adrian Paredes <adri@Adrians-MacBook.local>
2025-08-29 17:00:55 -0500

    Fix fuentes, supabase images y cliente

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from 'next/app';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
